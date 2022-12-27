5003cbbae98f56844b1bf3a49af753a26502bde2
Jorge I. Mendoza <121473205+jorge-mendoza-corella@users.noreply.github.com>
2022-12-27 04:46:41 +0000

    segundo push

--- variables.py
+++ variables.py
@@ -1,9 +1,10 @@
 print("aqui voy a empezar")
-
 '''empezando a 
 recolectar informacion'''
 
 miNombre = input("dame tu nombre")
 
 #aqui imprimo el ultimo nombre
-print('el nombre que puso es:',miNombre)
+print('el nombre que puso es:', miNombre)
+""" a hacer otra cosa
+"""
