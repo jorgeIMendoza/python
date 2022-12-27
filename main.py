e9b3f84d4793ba563b9e28ca45a1bedaf6005bdc
Jorge I. I <121473205+jorge-mendoza-corella@users.noreply.github.com>
2022-12-27 04:43:20 +0000

    primer commit

---
+++ main.py
@@ -0,0 +1,3 @@
+print('siasfj')
+#una c
+
---
+++ variables.py
@@ -0,0 +1,9 @@
+print("aqui voy a empezar")
+
+'''empezando a 
+recolectar informacion'''
+
+miNombre = input("dame tu nombre")
+
+#aqui imprimo el ultimo nombre
+print('el nombre que puso es:',miNombre)
